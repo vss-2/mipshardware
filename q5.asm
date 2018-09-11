2e5a9a82b8e8562018c3e2e837c2d9fe637e4d4b
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-09-11 10:59:33 -0300

    q5
    Correção na lógica utilizada

--- q5.asm
+++ q5.asm
@@ -1,38 +1,40 @@
 .data
-	ArrayA:	.word 1,2,3,4,5,6,7,8,9
-	ArrayB:	.space 100 
-	divs: .word
-    contador: .word 0
+	ArrayA:	.word 3, 2, 3, 4, 5, 6, 7, 8, 9
+	ArrayB:	.word 0, 0, 0, 0, 0, 0, 0, 0, 0 
+	  rems: .word 0
+      contador: .word 0
 	
 .globl main
 
+.text 
 main:
 
 	lw $t0, ArrayA	 #Array de entrada
 	lw $t1, ArrayB   #Array de saida
-	lw $t2, divs	 #Resultado da divisao
+	lw $t2, rems	 #Resultado da remisao
 	lw $t3, contador #Percorre o ArrayA
 	
 Laco:
-				#Varios branchs se nao for primo, se for salva no ArrayB
-	rem  $t2, $t0, 2
-	beq  $t2, 0, naoPrimo
-	rem  $t2, $t0, 3
-	beq  $t2, 0, naoPrimo
-	rem  $t2, $t0, 5
-	beq  $t2, 0, naoPrimo
-	rem  $t2, $t0, 7
-	beq  $t2, 0, naoPrimo
-	
-	j Primo
+	#Efetua varias divisoes, faz ands, se todas tiverem resto 1, é primo, 
+	#senão, and de algum 0 é sempre 0, e não é primo
+	rem  $a0, $t0, 2
+	rem  $a1, $t0, 3
+	rem  $a2, $t0, 5
+	rem  $a3, $t0, 7
+
+	and $t4, $a0, $a1
+	and $t5, $a2, $a3
+	and $t6, $t4, $t5
+	beq $t6, 0, naoPrimo
+	beq $t6, 1, Primo
+
 
 naoPrimo:
 	add $t3, $t3, 4
 	j Laco
 
 Primo:
-	sw $t1, ArrayB($t3)
+	sw $t1, ArrayB($t0)
 	j Laco
 	
 fimLaco:
-
