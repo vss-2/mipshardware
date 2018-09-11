ac2da5dbe227050fdb1f1a59bab2be32cc95688d
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-09-11 11:57:06 -0300

    q5
    Correção da lógica da questão e armazenamento do array

--- q5.asm
+++ q5.asm
@@ -3,24 +3,30 @@
 	ArrayB:	.word 0, 0, 0, 0, 0, 0, 0, 0, 0 
 	  rems: .word 0
       contador: .word 0
+   percorredor: .word 0
+      armazena: .word 0
 	
 .globl main
-
-.text 
-main:
+.text main:
 
 	lw $t0, ArrayA	 #Array de entrada
 	lw $t1, ArrayB   #Array de saida
 	lw $t2, rems	 #Resultado da remisao
-	lw $t3, contador #Percorre o ArrayA
+	lw $t3, contador #Percorre o ArrayB
+	lw $t4, percorredor #Percorre o ArrayA
+	lw $t5, armazena #Armazena o valor de ArrayA
 	
 Laco:
 	#Efetua varias divisoes, faz ands, se todas tiverem resto 1, é primo, 
 	#senão, and de algum 0 é sempre 0, e não é primo
+	
+	lw $t5, ArrayA($t4)
+	
 	rem  $a0, $t0, 2
 	rem  $a1, $t0, 3
 	rem  $a2, $t0, 5
 	rem  $a3, $t0, 7
+	add  $t4, $t4, 4
 
 	beq $a0, 0, naoPrimo
 	beq $a1, 0, naoPrimo
@@ -37,7 +43,8 @@ naoPrimo:
 	j Laco
 
 Primo:
-	sw $t1, ArrayB($t0)
+	sw $t1, ArrayB($t3)
+	add $t3, $t3, 4
 	j Laco
 	
 fimLaco:
