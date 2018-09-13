4ca159bd7ed7b39356f3d55735695cae4fe112f2
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-09-12 22:55:12 -0300

    q7
    Traduzindo o código

--- q7.asm
+++ q7.asm
@@ -1,56 +1,58 @@
 .data
 	valor: .word 13
 .text
+
 .globl main
+
 main:
 
-lw $v0, valor
-add $a0,$v0,$zero #move to $a0
+    lw $v0, valor
+    add $a0, $v0, $zero #RESPOSTA: sera salva em $a0
 
-jal fib #call fib
-add $a0,$v0,$zero
+    jal recfib #inicia a recursao
+    add $a0, $v0, $zero
 
-li $v0,10 #executa a saida do programa
-syscall
+    li $v0, 10 #executa a saida do programa
+    syscall
 
-fib:
-#a0=y
-#if (y==0) return 0;
-#if (y==1) return 1;
-#return( fib(y-1)+fib(y-2) );
+recfib:
 
-addi $sp,$sp,-12 #save in stack
-sw $ra,0($sp)
-sw $s0,4($sp)
-sw $s1,8($sp)
+    addi $sp, $sp, -12     #salva informacoes na pilha
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
 
-add $s0,$a0,$zero
+    add $s0, $a0, $zero
 
-addi $t1,$zero,1
-beq $s0,$zero,return0
-beq $s0,$t1,return1
+    addi $t1, $zero, 1
+    beq $s0, $zero, casozero
+    beq $s0, $t1, casoum
 
-addi $a0,$s0,-1
+    addi $a0,$s0,-1
 
-jal fib
+    jal recfib
 
-add $s1,$zero,$v0     #s1=fib(y-1)
+    add $s1, $zero, $v0     #s1 = fibonacci(n-1)
 
-addi $a0,$s0,-2
+    addi $a0, $s0, -2
 
-jal fib               #v0=fib(n-2)
+    jal recfib              #v0 = fibonacci(n-2)
 
-add $v0,$v0,$s1       #v0=fib(n-2)+$s1
-exitfib:
+    add $v0, $v0, $s1       #v0 = fibonacci(n-2) + $s1
 
-lw $ra,0($sp)       #read registers from stack
-lw $s0,4($sp)
-lw $s1,8($sp)
-addi $sp,$sp,12       #bring back stack pointer
-jr $ra
+pilhagem:
 
-return1:
- li $v0,1
- j exitfib
-return0 :     li $v0,0
- j exitfib
+    lw $ra, 0($sp)         #le os registradores da pilha
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    addi $sp, $sp, 12       #volta o apontador da pilha
+    jr $ra
+
+casoum:
+
+    li $v0, 1
+    j pilhagem
+casozero:     
+
+    li $v0, 0
+    j pilhagem
